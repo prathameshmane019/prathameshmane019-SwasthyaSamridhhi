89f79967b00b856c03a8f6617e7ea6826de7006b
Madhav Kamble <madhavukamble@gmail.com>
2024-03-08 19:55:09 +0530

    Sample Components added

---
+++ client/swasthyasamridhhi/components/FAQ.js
@@ -0,0 +1,33 @@
+export default function FAQ(){
+    return(
+        <>
+        <div className="collapse collapse-arrow bg-base-200">
+  <input type="radio" name="my-accordion-2" defaultChecked /> 
+  <div className="collapse-title text-xl font-medium">
+    We will use this for simple FAQ like what services do we provide and secure is your daa with our services and benefits of our application
+  </div>
+  <div className="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-arrow bg-base-200">
+  <input type="radio" name="my-accordion-2" /> 
+  <div className="collapse-title text-xl font-medium">
+    Click to open this one and close others
+  </div>
+  <div className="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-arrow bg-base-200">
+  <input type="radio" name="my-accordion-2" /> 
+  <div className="collapse-title text-xl font-medium">
+    Click to open this one and close others
+  </div>
+  <div className="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
+        </>
+    )
+}
